950ab73666b9e0b293dab76d6083a71d7d65dcad
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-07 23:39:23 +0530

    Area of circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class AreaOfCircle{
+ public static void main(String [] args){
+   Scanner sc=new Scanner(System.in);
+   double ra=sc.nextDouble();
+   double area=3.14*ra*ra;
+   System.out.println("Area of the circle :",area);
+ }
+}
