2ca63bea998a580962272c37d333f0fc67d3021a
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-07 23:41:54 +0530

    volume of cylinder

---
+++ VolumeOfCylinder.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class VolumeOfCylinder{
+ public static void main(String [] args){
+  Scanner sc=new Scanner(System.in);
+  double radius=sc.nextDouble();
+  double height=sc.nextDouble();
+  double volume=Math.PI* Math.pow(radius,2)*height;
+  System.out.println("volume of the cylinder:",volume);
+  
+  }
+}
